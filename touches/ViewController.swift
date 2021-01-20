78d940dc764deb21ead1bceee9005947d6b42bea
damir <yespeace@bk.ru>
2021-01-20 14:15:02 +0600

    Initial Commit

---
+++ touches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  touches
+//
+//  Created by yespeace on 20/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
